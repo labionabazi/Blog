fda3cceaed06a74ac07f3f8fda507991bf8f5307
labionabazi <labion.abazi@outlook.com>
2017-12-19 19:38:02 +0100

    entries complete

--- entries_public_complete.php
+++ entries_public_complete.php
@@ -3,7 +3,7 @@
   // Hier Code....
 $Entry = getEntry($EntryId);
   echo "<div><h4>".$Entry['title']." ".date("d.m.y",$Entry['datetime'])."</h4>";
-  echo "<p>".$Entry['content']."</p>";
+  echo "<p>".nl2br($Entry['content']."</p>";
   echo '<a href="index.php?function=entries_public&bid=';
   echo $blogId;
   echo '&eid=';
